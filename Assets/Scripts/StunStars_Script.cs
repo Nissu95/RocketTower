d6e7b8a00f03db6f4c43d2d8871b38d7768b3964
Nissu <nicosus95@gmail.com>
2018-12-08 15:13:45 -0300

    +Stars when stun

---
+++ Assets/Scripts/StunStars_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunStars_Script : MonoBehaviour {
+
+    [SerializeField] GameObject starsStun;
+    [SerializeField] ActorPlayer actorPlayer;
+
+    private void Update()
+    {
+        if (actorPlayer.isStunned)
+            starsStun.SetActive(true);
+        else
+            starsStun.SetActive(false);
+    }
+
+}
