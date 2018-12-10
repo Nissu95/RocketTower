6943fdcf1ae1358bc584f441a3c8650be00b890b
Nissu <nicosus95@gmail.com>
2018-12-10 16:56:02 -0300

    Dont vibrate in pause

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,12 +8,18 @@ public class PauseMenu : MonoBehaviour {
     [SerializeField] private GameObject pauseMenuCanvas;
     [SerializeField] GameObject resumeButton;
     private bool isPaused = false;
+    public InputListener[] AllInputs;
+    [SerializeField] VibrateJoystick vibrateJoystickOne;
+    [SerializeField] VibrateJoystick vibrateJoystickTwo;
+    [SerializeField] VibrateJoystick vibrateJoystickThree;
+    [SerializeField] VibrateJoystick vibrateJoystickFour;
 
     public void Start()
     {
         Time.timeScale = 1.0f;
         AllInputs = InputManager.Singleton.AllInputs;
     }
+
     public void OnDestroy()
     {
         Time.timeScale = 1.0f;
@@ -55,6 +61,18 @@ public class PauseMenu : MonoBehaviour {
     {
         isPaused = true;
         pauseMenuCanvas.SetActive(true);
+        if (vibrateJoystickOne.gameObject.activeInHierarchy)
+            vibrateJoystickOne.SetVibrating(0f, 0f, false);
+
+        if (vibrateJoystickTwo.gameObject.activeInHierarchy)
+            vibrateJoystickTwo.SetVibrating(0f, 0f, false);
+
+        if (vibrateJoystickThree.gameObject.activeInHierarchy)
+            vibrateJoystickThree.SetVibrating(0f, 0f, false);
+
+        if (vibrateJoystickFour.gameObject.activeInHierarchy)
+            vibrateJoystickFour.SetVibrating(0f, 0f, false);
+
         Time.timeScale = 0.0f;
         EventSystem.current.SetSelectedGameObject(resumeButton);
     }
@@ -74,7 +92,6 @@ public class PauseMenu : MonoBehaviour {
             Pause();
     }
 
-    public InputListener[] AllInputs;
     public void QuitToMainMenu()
     {
 		GameManager.SetActive (false);
